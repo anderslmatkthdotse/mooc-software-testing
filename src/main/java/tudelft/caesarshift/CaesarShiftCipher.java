093653f64420a65dddd362b823fd2f405267cb45
Anders <anderslm@kth.se>
2018-12-05 16:18:30 +0100

    Added tests for Caesar encryption

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -2,25 +2,30 @@ package tudelft.caesarshift;
 
 public class CaesarShiftCipher {
 
-    public String CaesarShiftCipher(String message, int shift){
+    public String encrypt(String message, int shift) {
         StringBuilder sb = new StringBuilder();
         char currentChar;
         int length = message.length();
 
-        shift = shift%26;
+        shift = shift % 26;
 
-        for(int i = 0; i < length; i++){
+
+        for (int i = 0; i < length; i++) {
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
+
             if (currentChar > 'z' || currentChar < 'a') {
                 return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
             }
-            sb.append((char) (currentChar + shift));
+
+            char encrChar = (char) (currentChar + shift);
+            if (encrChar > 'z') {
+                encrChar -= 26;
+            }
+            else if (encrChar < 'a') {
+                encrChar += 26;
+            }
+
+            sb.append(encrChar);
         }
 
         return sb.toString();
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,35 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CaesarShiftCipherTest {
 
+    @ParameterizedTest(name = "{0} shift {1} -> {2}")
+    @CsvSource({
+            "'ab3',0,'invalid'", // ugly - use exception
+            "'abc',0,'abc'",
+            "'abc',1,'bcd'",
+            "'abc',-1,'zab'",
+            "'xyz',1,'yza'",
+            "'xyz',27,'yza'",
+            "'abc',-27,'zab'"
+
+    })
+    public void testCaesar(String msg, int shift, String encrMsg) {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String result = csc.encrypt(msg, shift);
+        Assertions.assertEquals(encrMsg, result);
+    }
+
+    @Test
+    public void msgIsNull() {
+        assertThrows(NullPointerException.class, () -> {
+            String result = new CaesarShiftCipher().encrypt(null, 1);
+        });
+    }
 }
